5a6a764d51574bee3f2a306fbd46fd087f3158e5
Apple <apple@appurunoMacBook-Air-2.local>
2026-09-04 12:50:19 +0900

    Reorder featured members

--- app/data.ts
+++ app/data.ts
@@ -16,10 +16,10 @@ export const members = [
   { name: 'KEJAN', jp: 'ケジャン', role: 'リーダー・振付', image: '/assets/member-kejan.jpg' },
   { name: 'SUBARU', jp: 'スバル', role: 'サブリーダー・講師', image: '/assets/member-subaru.jpg' },
   { name: 'CHLOE', jp: 'クロエ', role: '映像編集・パフォーマー', image: '/assets/member-chloe.jpg' },
-  { name: 'HIRO', jp: 'ヒロ', role: 'サポートメンバー', image: '/assets/member-hiro.jpg' },
-  { name: 'AOZERO', jp: '青零', role: 'サポートメンバー', image: '/assets/member-seirei.jpg' },
   { name: 'SORYU', jp: 'ソリュ', role: 'パフォーマー', image: '/assets/member-soryu.jpg' },
   { name: 'YOSHIKI', jp: 'ヨシキ', role: 'パフォーマー', image: '/assets/member-yoshiki.jpg' },
+  { name: 'HIRO', jp: 'ヒロ', role: 'サポートメンバー', image: '/assets/member-hiro.jpg' },
+  { name: 'AOZERO', jp: '青零', role: 'サポートメンバー', image: '/assets/member-seirei.jpg' },
 ];
 
 export const history = [
